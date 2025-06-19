1cce2a0e137b16a0fd05c5f33de013569e00b41b
Mateus Orlandin Lorenzatti <mateusolorenzatti@gmail.com>
2025-06-18 22:57:23 -0300

    Alterados os dados de mock

--- app/core/db.py
+++ app/core/db.py
@@ -4,26 +4,26 @@ from app.core.models import Contato, CategoriaContato, Telefone, TipoTelefone
 db: List[Contato] = [
     Contato(
         id=1,
-        nome="João Silva",
+        nome="Alice",
         telefones=[
-            Telefone(numero="11987654321", tipo=TipoTelefone.movel),
-            Telefone(numero="1134567890", tipo=TipoTelefone.fixo)
+            Telefone(numero="51987654321", tipo=TipoTelefone.movel),
+            Telefone(numero="5134567890", tipo=TipoTelefone.fixo)
         ],
         categoria=CategoriaContato.familiar
     ),
     Contato(
         id=2,
-        nome="Empresa XYZ",
+        nome="Blablatech Corp.",
         telefones=[
-            Telefone(numero="1140004000", tipo=TipoTelefone.comercial)
+            Telefone(numero="112312312", tipo=TipoTelefone.comercial)
         ],
         categoria=CategoriaContato.comercial
     ),
     Contato(
         id=3,
-        nome="Ana Paula",
+        nome="Carl",
         telefones=[
-            Telefone(numero="11999998888", tipo=TipoTelefone.movel)
+            Telefone(numero="2199009900", tipo=TipoTelefone.movel)
         ],
         categoria=CategoriaContato.pessoal
     )
